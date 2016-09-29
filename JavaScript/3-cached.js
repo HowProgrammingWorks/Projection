4190ff447f648b03f019b436474da95b45c8fab0
tshemsedinov <timur.shemsedinov@gmail.com>
2016-09-29 23:51:42 +0300

    Refactored JavaScript examples, added cached projection

---
+++ JavaScript/3-cached.js
@@ -0,0 +1,44 @@
+let persons = [
+  { name: 'Marcus Aurelius', city: 'Rome', born: 121 },
+  { name: 'Victor Glushkov', city: 'Rostov on Don', born: 1923 },
+  { name: 'Ibn Arabi', city: 'Murcia', born: 1165 },
+  { name: 'Mao Zedong', city: 'Shaoshan', born: 1596 },
+  { name: 'Rene Descartes', city: 'La Haye en Touraine', born: 1596 }
+];
+
+let md = {
+  name: ['name'],
+  place: ['city', upper, s => '<' + s + '>'],
+  age: ['born', age]
+};
+
+function projection(meta) {
+  let keys = Object.keys(meta);
+  return obj => keys.reduce((hash, key) => (
+    hash[key] = meta[key].reduce(
+      (val, fn, i) => i === 0 ? obj[fn] : fn(val), null
+    ), hash
+  ), {});
+};
+
+let p1 = projection(md);
+let data = persons.map(p1);
+console.dir(data);
+
+function capitalize(s) {
+  return s.replace(/\w+/g, function(word) {
+    return word.charAt(0).toUpperCase() + word.substr(1).toLowerCase();
+  });
+}
+
+function upper(s) {
+  return typeof(s) === 'string' ? s.toUpperCase() : '';
+}
+
+function age(year) {
+  return new Date().getFullYear() - new Date(year + '').getFullYear();
+}
+
+function inc(x) {
+  return ++x;
+}
