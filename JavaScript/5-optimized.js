55d337365a4c517ad1e7a0b9a78f1d301d097d67
tshemsedinov <timur.shemsedinov@gmail.com>
2017-09-05 18:41:49 +0300

    Add new example

---
+++ JavaScript/5-optimized.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const persons = [
+  { name: 'Marcus Aurelius', city: 'Rome', born: 121 },
+  { name: 'Victor Glushkov', city: 'Rostov on Don', born: 1923 },
+  { name: 'Ibn Arabi', city: 'Murcia', born: 1165 },
+  { name: 'Mao Zedong', city: 'Shaoshan', born: 1893 },
+  { name: 'Rene Descartes', city: 'La Haye en Touraine', born: 1596 }
+];
+
+const md = {
+  name: ['name'],
+  place: ['city', s => '<' + s.toUpperCase() + '>'],
+  born: ['born'],
+  age: ['born', year => (
+      new Date().getFullYear() -
+      new Date(year + '').getFullYear()
+    )
+  ]
+};
+
+const projection = (meta) => {
+  const keys = Object.keys(meta);
+  return obj => {
+    const hash = {};
+    let name, key, def, val, fn;
+    for (key of keys) {
+      def = meta[key];
+      [name, fn] = def;
+      val = obj[name];
+      if (fn) val = fn(val);
+      hash[key] = val;
+    }
+    return hash;
+  };
+};
+
+const p1 = projection(md);
+const data = persons.map(p1);
+console.dir(data);
